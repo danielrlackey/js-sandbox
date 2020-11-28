916ff9347545107cfaa31c53125802c8435e4c0e
Daniel Lackey <danielrlackey0@gmail.com>
2020-11-28 09:14:37 -0500

    js basics

---
+++ conversion.js
@@ -0,0 +1,11 @@
+const firstName = "daniel";
+const lastName = "lackey";
+const age = 30;
+
+let val;
+
+val = firstName + " " + lastName;
+
+val = "hello my name is " + firstName + " and I am " + age;
+
+console.log(val);
