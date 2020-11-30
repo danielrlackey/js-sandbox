82447c8debad2c7dc89848ab894dcc121b3b9f16
Daniel Lackey <danielrlackey0@gmail.com>
2020-11-30 16:22:01 -0500

    playground

--- conversion.js
+++ conversion.js
@@ -1,31 +1,20 @@
-const numbers = [43, 56, 33, 23, 44, 36, 5];
-const numbers2 = new Array(22,45,33,76,54)
-const fruit = ["apple", "banna", "orange", "pear"];
-const mixed = [22, "hello", true, undefined, null, {a:1, b:1}, new Date()];
+person = {
+    firstName: "steve",
+    lastName: "everson",
+    age: 45,
+    email: "steveEverson@gmail.com",
+    hobbys: ["music", "food", "t.v"],
+    adress: {
+        city: "lincoln",
+        state: "nebraska",
+        street: "13th ave n",
+        aptNumber: "7546"
+    },
+    height: "6ft 2in"
+}
 
 let val;
 
-val = Array.isArray(numbers);
-val = numbers[3];
-val = numbers[0];
-val = numbers.push(44)
-val = numbers.indexOf(36);
-val = numbers.unshift(120);
-val = numbers.pop();
-numbers.shift();
-val = numbers.splice(1,1);
-numbers.reverse();
+val = person.firstName;
 
-val = numbers.concat(numbers2);
-val = fruit.sort();
-val = numbers.sort();
-
-val = numbers.sort((x,y) => {return x - y});
-
-const under50 = (num) => {
-    return num < 20;
-};
-val = numbers.find(under50);
-
-console.log(numbers);
-console.log(val);
+console.log(val)
